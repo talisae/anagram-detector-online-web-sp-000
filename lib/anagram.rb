789d46fcda82508f9c4ecc6077fe6a548c2cba9f
Talisa Erazo <talisaerazo@gmail.com>
2018-12-26 21:49:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,5 +4,10 @@ class Anagram
   def initialize(word)
     @word = word
   end
-   
+
+  def match(word_array)
+    word_array.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+  end
+
 end
